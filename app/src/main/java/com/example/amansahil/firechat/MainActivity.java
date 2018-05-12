2f385123afa3381ec0a672780efcc280a9a3d207
Aman Sahil <aman.salam2@gmail.com>
2018-05-12 12:01:06 +0400

    Stage 2

--- app/src/main/java/com/example/amansahil/firechat/MainActivity.java
+++ app/src/main/java/com/example/amansahil/firechat/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.amansahil.firechat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    EditText username;
+    Spinner lang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userEnter();
+    }
+
+    public void userEnter(){
+        b1 = findViewById(R.id.login_button);
+        username = findViewById(R.id.username);
+        lang = findViewById(R.id.lang);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = username.getText().toString();
+                String userLang = lang.getSelectedItem().toString();
+                String selectedLang = "";
+                switch(userLang) {
+                    case "Spanish":
+                        selectedLang = "es";
+                        break;
+                    case "French":
+                        selectedLang = "fr";
+                        break;
+                    case "Arabic":
+                        selectedLang = "ar";
+                        break;
+                    default:
+                        selectedLang = "en";
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/amansahil/firechat/MenuActivity.java
@@ -0,0 +1,4 @@
+package com.example.amansahil.firechat;
+
+public class MenuActivity {
+}
